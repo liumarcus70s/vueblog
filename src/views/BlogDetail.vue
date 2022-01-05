80c9ea49d79756bed60f32d3fcb7da1b91af3d5b
liumarcus70s <jz_leo@yeah.net>
2022-01-05 17:56:19 +0800

    second ed

---
+++ src/views/BlogDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>blogdetail</div>
+</template>
+
+<script>
+export default {
+  name: "BlogDetail"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/BlogEdit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="m-content">
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Header from "../components/Header";
+  export default {
+    name: "BlogEdit",
+    components: {Header}
+  }
+</script>
+
+<style scoped>
+
+</style>
